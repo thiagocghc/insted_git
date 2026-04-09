261bc075385fefb913c6f398c3c31b7bf82a2ed6
Thiago Almeida <thiagoalmeida@live.com>
2026-04-09 19:56:50 -0400

    alterações no projeto

--- projeto.py
+++ projeto.py
@@ -1,10 +1,14 @@
 
+print("Bem vindo ao programa em Python")
 
 print("Hello World")
 x = int(input("Digite quantas vezes voce quer no while: "))
-y = 20
+
+
 i = 0
 while i < x:
     print(i)
     i += 1
 
+
+print("fim")
